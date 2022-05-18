b7931c9d95c3f5878cf1be278321cef82c175e8f
Chia-Lung Chen <eragonj@eragonj.me>
2022-05-18 13:43:10 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# copicake-swift
+
+A description of this package.
---
+++ Sources/copicake-swift/copicake_swift.swift
@@ -0,0 +1,6 @@
+public struct copicake_swift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
